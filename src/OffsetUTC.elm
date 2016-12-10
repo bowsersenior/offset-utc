154e5a419e4c358c9732bfb323d255190751cd8d
Mani Tadayon <bowsersenior@gmail.com>
2016-12-09 17:52:44 -0800

    Add docs

--- src/OffsetUTC.elm
+++ src/OffsetUTC.elm
@@ -1,9 +1,18 @@
 module OffsetUTC exposing
   ( getTimezoneOffsetInMinutes )
 
+{-| This library exposes JavaScript's Date.prototype.getTimezoneOffset in Elm as `OffsetUTC.getTimezoneOffsetInMinutes`
+
+# Definition
+@docs getTimezoneOffsetInMinutes
+
+-}
+
 import Date exposing (Date)
 import Native.OffsetUTC
 
+{-| Get the UTC offset, in minutes, for a `Date`
+-}
 getTimezoneOffsetInMinutes : Date -> Int
 getTimezoneOffsetInMinutes = 
   Native.OffsetUTC.getTimezoneOffsetInMinutes
